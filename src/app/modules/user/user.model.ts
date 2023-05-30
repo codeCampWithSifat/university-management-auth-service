cba24795f6e50190bcf77cddc5dca7c788b0cb06
codeCampWithSifat <sayedhossainsifat300@.com>
2023-05-30 23:18:57 +0600

    user model and interface done

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,10 @@
+import { Schema, model } from 'mongoose'
+import { IUser } from './user.interface'
+
+const userSchema = new Schema<IUser>({
+  id: { type: String, required: true, unique: true },
+  role: { type: String, required: true },
+  password: { type: String, required: true },
+})
+
+export const User = model<IUser>('User', userSchema)
