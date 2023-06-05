3ee5cf89aa633e1eadf52d11104e9dcaacaf2425
codeCampWithSifat <sayedhossainsifat300@.com>
2023-06-05 22:15:14 +0600

    all error handler done

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,42 @@ import mongoose from 'mongoose'
 import config from './config/index'
 import app from './app'
 import { logger, errorLogger } from './share/logger'
+import { Server } from 'http'
 
+process.on('uncaughtException', error => {
+  errorLogger.error(error)
+  process.exit(1)
+})
+
+let server: Server
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Application Listening on port ${config.port}`)
     })
     logger.info('Database connected Successfully')
   } catch (err) {
     errorLogger.error('Database Error', err)
   }
+
+  process.on('unhandledRejection', error => {
+    console.log('Unhandled Rejection Is Detected')
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 main()
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTREM is received')
+  if (server) {
+    server.close()
+  }
+})
