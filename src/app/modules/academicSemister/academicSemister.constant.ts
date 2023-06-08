e4707982ed991871da70ecadb6af25d46d81fb31
codeCampWithSifat <sayedhossainsifat300@.com>
2023-06-09 00:39:52 +0600

    module 13 er 11 and 12 number video done

--- src/app/modules/academicSemister/academicSemister.constant.ts
+++ src/app/modules/academicSemister/academicSemister.constant.ts
@@ -30,3 +30,11 @@ export const academicSemesterCodes: IAcademicSemeterCodes[] = [
   '02',
   '03',
 ];
+
+export const academicSemesterTitleCodeMapper: {
+  [key: string]: string;
+} = {
+  Autumn: '01',
+  Summer: '02',
+  Fall: '03',
+};
--- src/app/modules/academicSemister/academicSemister.service.ts
+++ src/app/modules/academicSemister/academicSemister.service.ts
@@ -1,9 +1,15 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import { academicSemesterTitleCodeMapper } from './academicSemister.constant';
 import { IAcademisSemister } from './academicSemister.interface';
 import { AcademicSemester } from './academicSemister.model';
 
 const createSemester = async (
   payload: IAcademisSemister
 ): Promise<IAcademisSemister> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester Code');
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
